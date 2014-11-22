1e2d3dad1b98f6de1884674489fd2965b7139b5d
pierrebergesupelec <pierrebergesupelec@gmail.fr>
2014-11-22 12:39:13 +0100

    Ajout mutations
    Ajout de différentes mutations : 3opt Move, swapRandom et totalRandom.
    Il suffit, pour les tester, de changer de mutation dans la classe Recuit

--- TSPedge/Routage2.java
+++ TSPedge/Routage2.java
@@ -84,10 +84,10 @@ public class Routage2 {
 
 	
 		public String toString() {
-			int n = g.nombreDeNoeuds();
+			int n = this.tailleRoute();
 			String s = "";
 			for (int index = 0; index < n; index++){
-				s += route.get(index).intValue() + "("+ g.longueurEntre(route.get(index),getPreviousIndex(index)) + ")"+ "->";
+				s += route.get(index).intValue() + /*"("+ g.longueurEntre(route.get(index),getPreviousIndex(index)) + ")"+ */"->";
 			}
 			return s;
 		}
@@ -96,8 +96,19 @@ public class Routage2 {
 	public void swap(int i, int j){
 		Collections.swap(route,i,j);
 	}
-	
 	//une fois les getters setters crÃ©es on attaque les opÃ©rations sur les routes
+	//Mutation : on swape seulement 2 noeuds au hasard
+	public void swapRandom() {
+		int randIndex1=0;
+		int randIndex2=0;
+		while (randIndex1==randIndex2){
+		randIndex1 = (int) (tailleRoute() * Math.random());
+		randIndex2 = (int) (tailleRoute() * Math.random());
+		}
+		this.swap(randIndex1,randIndex2);
+	}
+	
+	
 	
 // Mutations
 	// en theorie une classe mutations devrait etre plus adaptÃ©e a notre solution rigide
@@ -120,6 +131,62 @@ public class Routage2 {
 			i=getNextIndex(i);
 			j=getPreviousIndex(j);
 		}
+	}
+	//Fonction nécessaire pour threeOptMove : elle réordonne trois nombres
+	public int[] order(int i, int j, int k){
+		int[] tab = new int[3];
+		if (i<j && i<k){
+			tab[0]=i;
+			tab[1]=Math.min(j,k);
+			tab[2]=Math.max(j,k);
+			return tab;
+		}
+		if (j<i && j<k){
+			tab[0]=j;
+			tab[1]=Math.min(i,k);
+			tab[2]=Math.max(i,k);
+			return tab;
+		}
+		tab[0]=k;
+		tab[1]=Math.min(i,j);
+		tab[2]=Math.max(i,j);
+		return tab;
+	}
+	// Attention ! Ici, le threeOptMove n'est valable que pour des graphes symétriques !
+	public void threeOptMove(){
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		ArrayList<Integer> l = new ArrayList<Integer>();
+		while (j==k || i==k || i==j){
+			i = (int) (tailleRoute() * Math.random()); 
+			j = (int) (tailleRoute() * Math.random()); 
+			k = (int) (tailleRoute() * Math.random());
+		}
+		int[] tab = order(i,j,k);
+		int step1=tab[0];
+		int step2=tab[1];
+		int step3=tab[2];
+		int index = step2;
+		while(index!=step3){
+			l.add(this.getRoute().get(index));
+			index=getNextIndex(index);
+		}
+		index=step1;
+		while(index!=step2){
+			l.add(this.getRoute().get(index));
+			index=getNextIndex(index);
+		}
+		index=step3;
+		while(index!=step1){
+			l.add(this.getRoute().get(index));
+			index=getNextIndex(index);
+		}
+		this.route = l;
+	}
+	//La mutation consiste à aller aléatoirement vers une autre route
+	public void totalRandom(){
+		this.route = routeInitiale();
 	}
 
 	public double getDistance(){
