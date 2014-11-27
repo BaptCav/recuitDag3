b1b9542f6bf2893ef4df3ff36e4a6388ede4628b
Petreius <pabelaube@hotmail.fr>
2014-11-27 01:11:06 +0100

    Update Routage2.java
    Contient désormais une initialisation greedy. Vérifiée main sur des petits graphes (bayg29, brazil58...)

--- TSP_edge/Routage2.java
+++ TSP_edge/Routage2.java
@@ -1,77 +1,140 @@
-import java.io.IOException;
-
-import javax.swing.*;
-
-// Cette classe dÃ©finit le problÃ¨me du recuit. Il se charge d'effectuer les mutations Ã©lÃ©mentaires, de calculer l'Ã©nergie et de diminuer T...
-
-public class Recuit extends JFrame
-{
-	private static final long serialVersionUID = 2L;
-	static double temperature = 10000;
-	static double refroidissement = 0.0001;
-	static double K = 1;
-
-
-
-
-	// ProbabilitÃ© d'accepter une solution pire que l'actuelle
-	public static double probaAcceptation(int energieCourante, int energieNouvelle, double temperature) 
-	{
-		// Si la nouvelle solution est meilleure, alors on accepte !
-		if (energieNouvelle < energieCourante) {
-			return 1.0;
+import java.util.ArrayList;
+import java.util.Collections;
+public class Routage2 {
+	ArrayList<Integer> route ;
+	Graphe g;
+	public ArrayList<Integer> routeInitiale() {
+		int n = g.nombreDeNoeuds();
+		ArrayList<Integer> liste = new ArrayList<Integer>();
+		for (int index = 0; index < n; index++) {
+			liste.add(new Integer(index));
 		}
-		// si elle est pire, on dÃ©finit une proba pour accepter Ã©ventuellement cette solution...
-		return Math.exp((energieCourante - energieNouvelle) / (1*temperature));
+		// rÃƒÂ©organise alÃƒÂ©atoirement l'ordre de visite
+		Collections.shuffle(liste);
+		return liste;
 	}
-
-	// Remonte la tempÃ©rature, remet tout en ordre !
-
-	// Cette mÃ©thode est la plus importante : elle implÃ©mente l'algorithme du recuit simulÃ©
-	public static Routage2 solution(Graphe g) throws IOException, InterruptedException
-	{
-		// On définit une route aléatoire en premier lieu
-		Routage2 solutionCourante = new Routage2(g);
-
-		// On en calcule l'énergie
-		System.out.println("distance solution initiale: " + solutionCourante.getDistance());
-		Thread.sleep(2000);
-		// et on dit que pour l'instant, c'est la meilleure route !
-		Routage2 meilleureRoute = new Routage2(g);
-		Routage2 nvelleSolution = new Routage2(g);
-		double temperatureRecuit = temperature;
-		// On répète tant que la température est assez haute
-		while (temperatureRecuit > 1) {
-			// On crée une nouvelle route conçue à partir de l'ancienne
-			nvelleSolution.clone(solutionCourante);
-			// Sur cette nouvelle route, on effectue une mutation élémentaire (2optMove)
-			nvelleSolution.twoOptMove();
-
-			// On récupère l'énergie (distance de parcours) des deux routes
-			int energieCourante = solutionCourante.getDistance();
-			int energieVoisine = nvelleSolution.getDistance();
-
-			// On décide si on accepte cette nouvelle route comme vu précédemment
-			if (probaAcceptation(energieCourante, energieVoisine, temperatureRecuit) >= Math.random()) {
-				solutionCourante.clone(nvelleSolution);
+	
+	public ArrayList<Integer> greedyRoute(int[][] matMin) {
+		int n = g.nombreDeNoeuds();
+		int prochainCandidat =0;
+		boolean noeudValide =false;
+		ArrayList<Integer> liste = new ArrayList<Integer>();
+		int colonneCandidate = 1;
+		liste.add(matMin[(int)((n)*Math.random())][1]); //Le premier point visité est le voisin le plus proche du point indexé 0	
+		for (int i = 1; i < n; i++) 
+		{
+			prochainCandidat = matMin[liste.get(i-1)][colonneCandidate];
+			for(int l = 0; l<liste.size() ;l++)
+			{
+				if(prochainCandidat == liste.get(l)) // si la prochaine ville est malheureusement deja dans la liste...
+				{
+					colonneCandidate+=1;
+					noeudValide = false;
+					prochainCandidat = matMin[liste.get(i-1)][colonneCandidate];
+					l = -1; // car la boucle la réincrémente automatiquement à 0 ! ne pas mettre 0 !
+				}
+				else
+				{
+					noeudValide = true;
+				}
 			}
-
-			if (solutionCourante.getDistance() < meilleureRoute.getDistance()) {
-				meilleureRoute.clone(solutionCourante);
-			
+			if(noeudValide == true)
+			{
+				liste.add(prochainCandidat);
+				noeudValide = false;
+				colonneCandidate = 1;
+				//System.out.println("i = "+i);
+			//	System.out.println("prochainCandidat = "+prochainCandidat);
 			}
-			// et si cette solution est meilleure que toutes les précédentes, alors on l'enregistre comme étant la meilleure pour l'instant
-			
-			System.out.println(" Energie " + solutionCourante.getDistance() + " Temperature " + temperatureRecuit);
-			//Thread.sleep(1000);
-			temperatureRecuit *= 1-refroidissement;
 		}
-		// Lorsque l'énergie cinétique n'est plus suffisante, on s'arrête et on affiche la solution trouvée
-		System.out.println("distance de la solution trouvee: " + meilleureRoute.getDistance());
-		System.out.println("Tour: " + meilleureRoute);
-		return meilleureRoute;
-
-
-
-	}   
+		
+		for (int l = 0; l < liste.size(); l++) {
+			//System.out.println("greedy route : "+liste.get(l));
+		}
+		return liste;
+	}
+	
+	public Routage2 (Graphe g, int[][] matMin){
+		this.g = g;
+		this.route=greedyRoute(matMin);
+	}
+	
+	public Routage2 (Graphe g){
+		this.g = g;
+		this.route=routeInitiale();
+	}
+	public Routage2 (Graphe g, ArrayList<Integer> liste){
+		this.g=g;
+		this.route=liste;
+	}
+	public void clone(Routage2 modele)
+	{
+		int n = g.nombreDeNoeuds();
+		for (int index = 0; index < n; index++){
+			this.getRoute().set(index, modele.route.get(index));
+		}
+	}
+	public void clear()
+	{
+		this.route.clear();
+	}
+	public Graphe getGraphe(){
+		return this.g;
+	}
+	// En entrée, un indice correspondant à la place d'un élément sur une route. En sortie, le noeud correspondant à l'indice suivant
+	public int getNext(int index){
+		if (index==this.tailleRoute()-1) {
+			return this.getRoute().get(0);
+		} else {
+			return this.getRoute().get(index+1);
+		}
+	}
+	//En entrée, un indice correspondant à la place d'un élément sur une route. En sortie, l'indice correspondant au noeud suivant(dans la plupart des cas, on ajoute simplement 1)
+	public int getNextIndex(int index){
+		if (index==(this.tailleRoute()-1)) {
+			return 0;
+		} else {
+			return (index+1);
+		}
+	}
+	// En entrée, un indice correspondant à la place d'un élément sur une route. En sortie, le noeud correspondant à l'indice précédent
+	public int getPrevious(int index){
+		if (index==0) {
+			return this.getRoute().get(this.tailleRoute() - 1);
+		} else {
+			return this.getRoute().get(index-1);
+		}
+	}
+	// En entrée, un indice correspondant à la place d'un élément sur une route. En sortie, l'indice du noeud précédent(en général, on soustrait 1)
+	public int getPreviousIndex(int index){
+		if (index==0) {
+			return (this.tailleRoute() - 1);
+		} else {
+			return (index-1);
+		}
+	}
+	public ArrayList<Integer> getRoute(){
+		return route;
+	}
+	public int tailleRoute() {
+		return route.size();
+	}
+	public String toString() {
+		int n = this.tailleRoute();
+		String s = "";
+		for (int index = 0; index < n; index++){
+			s += route.get(index).intValue() + /*"("+ g.longueurEntre(route.get(index),getPreviousIndex(index)) + ")"+ */"->";
+		}
+		return s;
+	}
+	public double getDistance(){
+		double cpt=0.0;
+		int j=0;
+		int L = this.tailleRoute();
+		for(int i=0;i<L;i++){
+			j=this.getNextIndex(i);
+			cpt+=this.getGraphe().getdists()[this.getRoute().get(i)][this.getRoute().get(j)];
+		}//on prend quand meme en consideration la distance dernier -> premier
+		return cpt;
+	}
 }
