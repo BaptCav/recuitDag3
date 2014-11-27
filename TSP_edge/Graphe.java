7a3ff7930009ce3871ed69c2fb14e40299594f4e
Petreius <pabelaube@hotmail.fr>
2014-11-27 01:04:11 +0100

    Update Graphe.java

--- TSP_edge/Graphe.java
+++ TSP_edge/Graphe.java
@@ -1,22 +1,19 @@
+import java.util.Arrays;
 
 public class Graphe {
-
 	// Un graphe est défini par une matrice carrée représentant les distances entre chaque noeud
-	int[][] longueursAretes;
-	
+	double[][] longueursAretes;
 	//Constructeur
-	public Graphe(int[][] tab) {
+	public Graphe(double[][] tab) {
 		longueursAretes = tab;
 	}
-	public int[][] getdists(){
+	public double[][] getdists(){
 		return this.longueursAretes;
 	}
-	
 	//Fournit la longueur entre 2 noeuds
 	public double longueurEntre(int index1, int index2) {
 		return longueursAretes[index1][index2];
 	}
-	
 	//Renvoie le nombre de noeuds du graphe
 	public int nombreDeNoeuds() {
 		return longueursAretes.length;
