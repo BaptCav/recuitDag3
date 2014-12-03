f5cde2f50a2d39a7e1a447687c3b408e506aec8e
BaptCav <Baptiste.cavarec@orange.fr>
2014-12-03 12:09:41 +0100

    j'avais oublié de boucler...

--- Quantique/EnergieCinetiqueTsp.java
+++ Quantique/EnergieCinetiqueTsp.java
@@ -15,7 +15,16 @@ public class EnergieCinetiqueTsp extends EnergieCinetique {
 				}
 			}	
 		}
+		// on boucle les répliques 
+		int[][] Mi = r[r.length-1].toIsing();
+		int[][] Mj = r[1].toIsing();
+		for(int k =0;k<Mi.length;k++){
+			for(int l =0;l<Mi[0].length;l++){
+				compteurspinique+=Mi[k][l]*Mj[k][l];
+			}
+		}	
 		
+		//puis on retourne la somme des produits de spins pondérés pas la fonction de pondération
 		return Ponderation.calcul(p.getT())*compteurspinique;
 	}
 
