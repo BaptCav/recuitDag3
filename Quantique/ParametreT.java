8a652d99654f120dbf64e2829fb63cdebf0d53ae
pierrebergesupelec <pierrebergesupelec@gmail.fr>
2014-12-12 14:23:15 +0100

    Ajout de l'argument TemperatureFin

--- Quantique/ParametreT.java
+++ Quantique/ParametreT.java
@@ -1,14 +1,17 @@
 
+
 public class ParametreT {
 
 	private double temperature;
 	private double facteurDeRefroidissement;
+	private double temperaturefin;
 	
-	public ParametreT(double temp,double froid){
+	public ParametreT(double temp,double froid,double temperaturefin){
 		this.setTemperature(temp);
 		this.facteurDeRefroidissement = froid;
+		this.temperaturefin = temperaturefin;
 	}
-
+// Get et Set de la temperature initiale
 	public double getTemperature() {
 		return temperature;
 	}
@@ -16,8 +19,21 @@ public class ParametreT {
 	public void setTemperature(double temperature) {
 		this.temperature = temperature;
 	}
-	public void refroidissement(){
+//Get et Set de la temperature de fin	
+	public double getTemperatureFin(){
+		return temperaturefin;
+	}
+	
+	public void setTemperatureFin(double temperaturefin){
+		this.temperaturefin = temperaturefin;
+	}
+//Get et Set du refroidissement, refroidissement de la temperature Recuit en exponentiel et lineaire
+	public void refroidissementExp(){
 		this.temperature *= 1-this.facteurDeRefroidissement;
+	}
+	
+	public void refroidissementLin(){
+		this.temperature -= this.facteurDeRefroidissement;
 	}
 
 	public double getFacteurDeRefroidissement() {
@@ -30,3 +46,4 @@ public class ParametreT {
 	
 	
 }
+
