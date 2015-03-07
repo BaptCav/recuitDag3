8b92824afaf7722d67d67326383be0ac2c72c152
pierrebergesupelec <pierrebergesupelec@gmail.fr>
2015-03-07 17:48:09 +0100

    Test Javadoc
    Test

--- Quantjar/src/parametrage/EnergieCinetique.java
+++ Quantjar/src/parametrage/EnergieCinetique.java
@@ -4,6 +4,13 @@ import modele.Particule;
 
 public class EnergieCinetique {
 public static double calculer(Particule p){	
+	/**
+	 * N'est pas utilisé en tant que tel. 
+	 * On calcule toujours une sous-énergie cinétique qui correspond à un certain problème
+	 * Ex : EnergieCinetiqueTSP pour TSP
+	 * @param la particule sur laquelle on calcule l'énergie cinétique
+	 * @return 0
+	 */
 	return 0;
 }
 }
